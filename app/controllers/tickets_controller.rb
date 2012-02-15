6f4482dc6659ac692fc7a724b562576d7e563733
Jinpu Hu <hujinpu@gmail.com>
2012-02-15 16:29:25 +0800

    Restricting ticket updating to only those who have permission

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -8,6 +8,7 @@ class TicketsController < ApplicationController
                                         :destroy]
 
   before_filter :authorize_create!, :only => [:new, :create]
+  before_filter :authorize_update!, :only => [:edit, :update]
 
 
   def new
@@ -69,6 +70,13 @@ class TicketsController < ApplicationController
   def authorize_create!
     if !current_user.admin? && cannot?("create tickets".to_sym, @project)
       flash[:alert] = "You cannot create tickets on this project."
+      redirect_to @project
+    end
+  end
+
+  def authorize_update!
+    if !current_user.admin? && cannot?(:"edit tickets", @project)
+      flash[:alert] = "You cannot edit tickets on this project."
       redirect_to @project
     end
   end
